ac79b97b88e72dc4492869b66cf785db35d8f77f
Harry Clark <139020900+astonHC@users.noreply.github.com>
2025-10-27 14:53:26 +0000

    Update pso.h

--- PSO/cw/pso.h
+++ PSO/cw/pso.h
@@ -33,7 +33,46 @@
 
     #define         PSO_MAX_PARTICLES           50
     #define         PSO_MAX_ITER                1000
-    #define         PSO_MAX_DIM                 14              // ACCOUNTING FOR 1 BIAS FOR EVERY 13 WEIGHTS FOR DEMAND 
+    #define         PSO_MAX_DIM                 14              // ACCOUNTING FOR 1 BIAS FOR EVERY 13 WEIGHTS FOR DEMAND
+    #define         PSO_MAX_CONV                20
+    
+    #define         PSO_MAX_INERTIA             1.2
+    #define         PSO_MIN_INERTIA             0.9
+    #define         PSO_COG_INIT                2.5
+    #define         PSO_COG_FIN                 0.5
+    #define         PSO_SOC_INIT                0.5
+    #define         PSO_SOC_FIN                 2.5
+    #define         PSO_DIV                     0.01
+
+    #define         PSO_RAND()                  ((double)rand()/ (double)RAND_MAX)
+    #define         PSO_SEED()                  srand((unsigned)time(NULL))
+
+    // DEFINE THE BASIS FOR THE ERROR HANDLING 
+    // EXCLUSIVE TO THIS IMPLEMENTATION OF PSO
+    typedef enum
+    {
+        PSO_ERROR_NONE = 0,
+        PSO_ERROR_OOB,
+        PSO_ERROR_PARTICLE,
+        PSO_ERROR_CONVERGENCE,
+        PSO_ERROR_FITNESS,
+        PSO_ERROR_DIM,
+        PSO_ERROR_FILE,
+        PSO_ERROR_MEM
+
+    } PSO_ERROR_TYPE;
+
+    static const char* PSO_ERR[] =
+    {
+        "OK",
+        "INDEX OUT OF BOUNDS",
+        "PARTICLE ERROR",
+        "CONVERGENCE ERROR",
+        "FITNESS ERROR",
+        "DIMENSION ERROR",
+        "FILE IO ERROR",
+        "MEMORY ERROR"
+    };
 
 #endif
 #endif
